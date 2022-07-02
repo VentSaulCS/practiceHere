ce94a9b7e70ad75da0f9adeb7ecc72f5dce6556e
Saul Ventura <VentSaulCS@outlook.com>
2022-07-02 17:50:11 -0500

    update

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+// GLOBALS - NO WINDOW !!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require    - function to use modules (commonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+//You will always have access to Global varaibles on the page.
+
+console.log(__dirname)
